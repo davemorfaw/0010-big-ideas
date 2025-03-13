1f4f65384e186ac815b91345a2fccefca8a52725
Dave <davemorfaw1@gmail.com>
2025-03-13 16:29:03 -0400

    basic

---
+++ 0010 big ideas.py
@@ -0,0 +1,14 @@
+def main():
+    # Take name input
+    name = input("Enter your name: ")
+    
+    # Take another input 
+    other_input = input("Enter a number: ")
+    
+  
+        
+    print(f"Hello, {name}! Your age is {other_input}.")
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # 0010-big-ideas
+
+In this program, you are prompted for your name and age
+The program will print your name and age
